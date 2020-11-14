3080b6af5d7ff6470c60b4dd22fd823dc7b6413e
ExorBray <60280972+ExorBray@users.noreply.github.com>
2020-11-13 23:39:24 -0600

    Me equivoqué en algo xd

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ var menuState =
        game.camera.flash("#fff", 1200);
         this.spacebar;
 	    this.spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        var pressbtn = game.add.text(1000/ 2, 800 / 2, "Preciona Space para comenzar" , {font:"45px Console"});
+        var pressbtn = game.add.text(1000/ 2, 800 / 2, "Presione space para cempezar" , {font:"45px Console"});
 
         pressbtn.addColor("#fff",0);
         pressbtn.anchor.setTo(0.5, 0.5);
